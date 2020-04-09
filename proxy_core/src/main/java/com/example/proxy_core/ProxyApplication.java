11120c30443f7e3861d78225fe0cd949c131cc19
Neo <c.min.tseng@gmail.com>
2020-04-09 17:54:35 +0800

    modify dependencies class <-- fail crash ( java.lang.ClassNotFoundException)

--- proxy_core/src/main/java/com/example/proxy_core/ProxyApplication.java
+++ proxy_core/src/main/java/com/example/proxy_core/ProxyApplication.java
@@ -10,6 +10,9 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.gmail.yang1001yk.utils.EncryptUtil;
+import com.gmail.yang1001yk.utils.Zip;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
