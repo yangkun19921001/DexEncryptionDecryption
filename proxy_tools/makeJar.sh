1f2875ac0bd660de648b88ff1fa89f625e0e1ce7
DevYK <yangkun1001yk@gmail.com>
2020-04-11 21:10:50 +0800

    fix utils/ FileNotFoundException

---
+++ proxy_tools/makeJar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "start make jar"
+
+jar -cvfM classes.jar .
+
+echo "make jar successful"
+
--- proxy_tools/src/main/java/com/example/proxy_tools/Main.java
+++ proxy_tools/src/main/java/com/example/proxy_tools/Main.java
@@ -5,6 +5,7 @@ import com.gmail.yang1001yk.utils.EncryptUtil;
 import com.gmail.yang1001yk.utils.Zip;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -16,17 +17,22 @@ public class Main {
     /**
      * 制作 Dex 命令
      */
-    private static String DX_PATH = "/Users/Shared/sdk/build-tools/29.0.3/dx --dex --output ";
+    private static String DX_PATH = "/Users/devyk/Data/Android/SDK/build-tools/29.0.2/dx --dex --output ";
 
     /**
      * 制作 对齐 命令
      */
-    private static String ZIPALIGN = "/Users/Shared/sdk/build-tools/29.0.3/zipalign -v -p  4 ";
+    private static String ZIPALIGN = "/Users/devyk/Data/Android/SDK/build-tools/29.0.2/zipalign -v -p  4 ";
 
     /**
      * 制作 签名打包 命令
      */
-    private static String APKSIGNER = "/Users/Shared/sdk/build-tools/29.0.3/apksigner sign --ks ";
+    private static String APKSIGNER = "/Users/devyk/Data/Android/SDK/build-tools/29.0.2/apksigner sign --ks ";
+
+    /**
+     * JAVA_HOME
+     */
+    private static String JAVA_HOME = "/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/";
 
     /**
      * 记录执行制作 dex 的次数，第一次执行成功但是没有生成
@@ -43,49 +49,112 @@ public class Main {
             ZIPALIGN = "D:\\SDK\\build-tools\\29.0.3\\zipalign -v -p  4 ";
             APKSIGNER = "D:\\SDK\\build-tools\\29.0.3\\apksigner sign --ks ";
         }
-        /**
-         * 1.制作只包含解密代码的dex文件
-         */
-        makeDecodeDex();
 
         /**
-         * 2.加密APK中所有的dex文件
+         * 1. 多个 jar 合并在一起，目的是制作 dex 文件
+         */
+        mergeJar();
+
+        /**
+         * 2.制作只包含解密代码的dex文件
+         */
+        makeDecodeDex("proxy_tools/temp/classes.jar", "classes.jar", "proxy_tools/temp/", "classes.dex");
+
+        /**
+         * 3.加密APK中所有的dex文件
          */
         encryptApkAllDex();
 
         /**
-         * 3.把dex放入apk解压目录，重新压成apk文件
+         * 4.把dex放入apk解压目录，重新压成apk文件
          */
         makeApk();
         /**
-         * 4.对齐
+         * 5.对齐
          */
         zipalign();
         /**
-         * 5. 签名打包
+         * 6. 签名打包
          */
         jksToApk();
+    }
+
+    /**
+     * 这一步的目的是因为打包 utils 的时候没有将 utils.jar makeDex ,
+     * 所以我们不能直接加载 class 的 jar ，而是需要将 class 转化成 dex 字节码。
+     * 不然 utils 包下的 class 都会报 ClassNotFoundException
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static void mergeJar() throws IOException, InterruptedException {
+        System.out.println("start merge jar");
+        File file = new File("proxy_tools/temp/");
+        if (file.exists())
+            file.delete();
+        file.mkdir();
+        /**
+         * 1.1 解压 utils
+         */
+        unZipFile("utils/build/libs/utils-0.1.0.jar", "proxy_tools/temp/");
+        /**
+         * 1.2. 解压 core 库
+         */
+        unZipFile("proxy_core/build/outputs/aar/proxy_core-debug.aar", "proxy_tools/temp/");
+
+
+        /**
+         * 1.3 解压核心 jar 目的是为了将 core jar 和 工具包 jar 合并一个 jar
+         */
+        unZipFile("proxy_tools/temp/classes.jar", "proxy_tools/temp/");
+
+        /**
+         * 1.4 解压完成，需要删除 jar ,目的为了合并
+         */
+        Zip.deleteFile(new File("proxy_tools/temp/classes.jar"));
+
+        /**
+         * 1.5 开始合并
+         * 第三个参数指定在 temp 下执行打包
+         *
+         */
+        final Process process = Runtime.getRuntime().exec(JAVA_HOME + "jar -cvfM classes.jar .", null, new File("/Users/devyk/Data/Project/sample/github_code/DexEncryptionDecryption/proxy_tools/temp"));
+        process.waitFor();
+        if (process.exitValue() != 0) {
+            throw new RuntimeException("make jar error");
+        }
+        System.out.println("merge jar successful!");
+    }
+
+    private static void unZipFile(String jarPath, String tempFilePath) throws FileNotFoundException {
+        File unZipFile = new File(jarPath);
+        File tempFile = new File(tempFilePath);
+        checkFile(unZipFile, tempFile);
+        Zip.unZip(unZipFile, tempFile, false);
+    }
+
+    private static void checkFile(File unZipFile, File tempFile) throws FileNotFoundException {
+        if (!unZipFile.exists() || !tempFile.exists())
+            throw new FileNotFoundException(unZipFile.getAbsolutePath() + " or " + tempFile.getAbsolutePath());
     }
 
 
     /**
      * 1.制作只包含解密代码的dex文件
      */
-    public static void makeDecodeDex() throws IOException, InterruptedException {
+    public static void makeDecodeDex(String jarPath, String jarName, String dexPath, String dexName) throws IOException, InterruptedException {
         if (count >= 2) return;
         System.out.println("makeDecodeDex start");
-        File aarFile = new File("proxy_core/build/outputs/aar/proxy_core-debug.aar");
-        File aarTemp = new File("proxy_tools/temp");
-        Zip.unZip(aarFile, aarTemp);
-        File classesJar = new File(aarTemp, "classes.jar");
-        File classesDex = new File(aarTemp, "classes.dex");
+        File classesJar = new File(jarPath);
+        File classesDex = new File(dexPath);
+        checkFile(classesJar, classesDex);
         //dx --dex --output out.dex in.jar
         //dx --dex --output D:\Downloads\android_space\DexDEApplication\proxy_tools\temp\classes.dex D:\Downloads\android_space\DexDEApplication\proxy_tools\temp\classes.jar
         //       Windows 执行
 //         Process process = Runtime.getRuntime().exec("cmd /c dx --dex --output " + classesDex.getAbsolutePath()+ " " + classesJar.getAbsolutePath());
         //MAC 执行
 //        Process process = Runtime.getRuntime().exec(DX_PATH + classesDex.getAbsolutePath() + " " + classesJar.getAbsolutePath());
-        final String args = classesDex.getAbsolutePath() + " " + classesJar.getAbsolutePath();
+        String args = classesDex.getAbsolutePath() + File.separator + dexName + " " + classesJar.getAbsolutePath();
         String command = DX_PATH + args;
         if (isWindwos) {
             command = "cmd /c " + DX_PATH + args;
@@ -95,8 +164,7 @@ public class Main {
         if (process.exitValue() != 0) {
             throw new RuntimeException("dex error");
         }
-
-        if (!classesDex.exists()) makeDecodeDex();
+        if (!classesDex.exists()) makeDecodeDex(jarPath, jarName, dexPath, dexPath);
         System.out.println("makeDecodeDex--ok");
         count++;
     }
@@ -108,7 +176,7 @@ public class Main {
         System.out.println("encryptApkAllDex start");
         File apkFile = new File("app/build/outputs/apk/debug/app-debug.apk");
         File apkTemp = new File("app/build/outputs/apk/debug/temp");
-        Zip.unZip(apkFile, apkTemp);
+        Zip.unZip(apkFile, apkTemp, true);
         //只要dex文件拿出来加密
         File[] dexFiles = apkTemp.listFiles(new FilenameFilter() {
             @Override
